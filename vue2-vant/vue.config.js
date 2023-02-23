33fff48d945a6f691ad70bc3235d6333759890cb
Minf97 <287728237@qq.com>
2023-02-23 15:58:21 +0800

    uniapp-vant

---
+++ vue2-vant/vue.config.js
@@ -0,0 +1,11 @@
+  
+module.exports = {  
+  chainWebpack: (config) => {
+    config.module 
+      .rule("mjs$")
+      .test(/\.mjs$/)
+      .include.add(/node_modules/)
+      .end()
+      .type("javascript/auto");
+  },
+};
